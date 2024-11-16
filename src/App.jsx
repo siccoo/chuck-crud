35d16b8900534443a5d479378463d14e9ff9fd0e
Chilaka Michael Obinna <obichilaka@gmail.com>
2024-11-16 16:32:09 +0100

    updated UserList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import UserList from "./components/features/users/UserList";
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto px-2 pt-10 md:pt-32 max-w-5xl">
+      <h3 className="text-center text-2xl text-gray-700 font-bold">Redux Toolkit CRUD App</h3>
       <UserList />
-    </>
+    </div>
   );
 }
 
