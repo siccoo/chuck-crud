89d8f749ce99f03ffcf0fa0ae3df1126140d7cbb
Chilaka Michael Obinna <obichilaka@gmail.com>
2024-11-16 17:13:56 +0100

    added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import UserList from "./features/users/UserList";
 
 function App() {
   return (
     <div className="container mx-auto px-2 pt-10 md:pt-10 max-w-5xl">
       <h3 className="text-center text-2xl text-gray-700 font-bold pb-10">Redux Toolkit CRUD App</h3>
-      <UserList />
+      <Routes>
+        <Route path="/" element={<UserList />} />
+      </Routes>
+      
     </div>
   );
 }
